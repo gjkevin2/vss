e45f4ca8fafbe17b036d30ded8ef4841f0c8c528
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2020-12-01 09:23:40 +0800

    Update install-v2ray.sh

--- install-v2ray.sh
+++ install-v2ray.sh
@@ -96,7 +96,8 @@ systemctl enable v2ray
 
 wget -O /usr/share/nginx/html/static/config.yaml https://raw.githubusercontent.com/gjkevin2/vss/master/config.yaml
 sed -i 's/serverip/'$serverip'/g' /usr/share/nginx/html/static/config.yaml
-sed -i 's/serverdomain/'$domain'/g' /usr/share/nginx/html/static/config.yaml
+wget -O /usr/share/nginx/html/static/config.json https://raw.githubusercontent.com/gjkevin2/vss/master/config.json
+sed -i 's/serverip/'$serverip'/g' /usr/share/nginx/html/static/config.json
 
 #生成ss，vmess订阅
-bash creat-ref.sh $serverip
+bash create-ref.sh $serverip
