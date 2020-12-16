0ac037f864e86558eced5b5213b28a68f6cd8f7c
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2020-12-16 14:30:22 +0800

    Add files via upload

---
+++ install-sslibev.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apt -y install snapd haveged gawk
+snap install core shadowsocks-libev
+ipaddr=$(ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}')
+cat > /snap/bin/config.json <<-EOF
+{
+    "server": "$ipaddr",
+    "server_port": 8388,
+    "password": "barfoo!",
+    "method": "chacha20-ietf-poly1305",
+}
+EOF
+
+cat > /lib/systemd/system/ss.service <<-EOF
+[Unit]
+Description=Shadowsocks Server
+After=network.target
+
+[Service]
+Restart=on-abnormal
+ExecStart=/snap/bin/shadowsocks-libev.ss-server -c /snap/bin/config.json > /dev/null 2>&1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl start ss.service
+systemctl enable ss.service
