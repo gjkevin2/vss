1cef6ea705de0f00c9e883262d98349fd252a588
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 16:05:31 +0800

    Update nginxconf.sh

--- nginxconf.sh
+++ nginxconf.sh
@@ -15,13 +15,12 @@ server {
         proxy_pass https://imeizi.me;
     }
 
-    # 转发wss协议
-    location /uri {
+    # 拦截websocket请求
+    location /websocket {
         proxy_pass http://127.0.0.1:9000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
-        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header Connection "upgrade";
     }
 }
 EOF
