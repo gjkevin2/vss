f96647767bff3307649913f51755cdec80ebcf3a
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 15:44:54 +0800

    Update install-ssrust.sh

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -48,18 +48,18 @@ EOF
 cat >/etc/nginx/conf.d/ssrust.conf<<-EOF
 server {
     listen              80;
-    listen              443 ssl;
+    #listen              443 ssl;
     server_name         sli.flyrain.tk;
-    ssl_certificate     /root/cert/fullchain.cer;
-    ssl_certificate_key /root/cert/privkey.key;
-    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers         HIGH:!aNULL:!MD5;
+    #ssl_certificate     /root/cert/fullchain.cer;
+    #ssl_certificate_key /root/cert/privkey.key;
+    #ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
+    #ssl_ciphers         HIGH:!aNULL:!MD5;
 
  # 转发https协议
     location / {
-        proxy_pass https://imeizi.me;
-        proxy_set_header   X-Real-IP        $remote_addr;
-        proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
+        proxy_pass  https://imeizi.me;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }
 
     # 转发wss协议
