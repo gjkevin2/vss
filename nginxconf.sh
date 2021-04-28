51a02b62ca1409347c3faba1ad7581d90021c13f
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 16:02:08 +0800

    Update nginxconf.sh

--- nginxconf.sh
+++ nginxconf.sh
@@ -7,21 +7,24 @@ server {
     server_name         sli.flyrain.tk;
     ssl_certificate     /root/cert/fullchain.cer;
     ssl_certificate_key /root/cert/privkey.key;
-    #ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
-    #ssl_ciphers         HIGH:!aNULL:!MD5;
+    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers         HIGH:!aNULL:!MD5;
 
     location / {
         proxy_ssl_server_name on;
         proxy_pass https://imeizi.me;
     }
 
- # 转发https协议
-    # location / {
-    #     proxy_pass  https://imeizi.me;
-    #     proxy_set_header X-Real-IP $remote_addr;
-    #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    # }
+    # 转发wss协议
+    location /uri {
+        proxy_pass http://127.0.0.1:9000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header X-Real-IP $remote_addr;
+    }
 }
 EOF
 systemctl daemon-reload
-systemctl restart nginx
+systemctl stop nginx
+systemctl start nginx
