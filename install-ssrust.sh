e19a79709116495a8adb222bb95af81256283ca9
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 15:22:22 +0800

    Update install-ssrust.sh

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -55,20 +55,21 @@ server {
     ssl_ciphers ECDHE-RSA-AES128-GCM-SHA256:ECDHE:ECDH:AES:HIGH:!NULL:!aNULL:!MD5:!ADH:!RC4;
     ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
     ssl_prefer_server_ciphers on;
-    location / {
-        proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+ # 转发https协议
+    location /{
         proxy_pass https://imeizi.me;
+        proxy_set_header   X-Real-IP        $remote_addr;
+        proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
     }
+
+    # 转发wss协议
     location /uri {
-        proxy_set_header Host $host;   
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-F $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
         proxy_pass http://127.0.0.1:9000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header X-Real-IP $remote_addr;
     }
 }
 EOF
