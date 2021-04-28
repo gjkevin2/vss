055a278f718b99f2f1ed592d7082c2ca1887a5b4
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 16:57:51 +0800

    Update nginxconf.sh

--- nginxconf.sh
+++ nginxconf.sh
@@ -19,9 +19,8 @@ server {
     location /websocket {
         proxy_pass http://127.0.0.1:9000;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $http_host;
     }
 }
 EOF
