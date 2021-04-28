318f8f577a7d6c63b59163555b7da2e9eee4b716
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 12:09:15 +0800

    Update install-ssrust.sh

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~
+rm -rf shadowsocks-v* v2ray-plugin*
 lurl='https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest'
 latest_version=`curl $lurl| grep tag_name |awk -F '[:,"v]' '{print $6}'`
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v${latest_version}/shadowsocks-v${latest_version}.x86_64-unknown-linux-gnu.tar.xz
