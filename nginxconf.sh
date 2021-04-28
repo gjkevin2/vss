e8df54e40ef8813a6a5d8add489baf774490f283
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-04-28 15:57:18 +0800

    Update nginxconf.sh

--- nginxconf.sh
+++ nginxconf.sh
@@ -21,6 +21,7 @@ server {
     #     proxy_set_header X-Real-IP $remote_addr;
     #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     # }
+}
 EOF
 systemctl daemon-reload
-systemctl reload nginx
+systemctl restart nginx
