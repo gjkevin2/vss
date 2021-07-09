4b0e6759883b5bbefab0eb750bf5c318e679a76b
gjkevin2 <gjkevin2@163.com>
2021-07-09 11:21:15 +0800

    update

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -6,12 +6,12 @@ latest_version=`curl $lurl| grep tag_name |awk -F '[:,"v]' '{print $6}'`
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v${latest_version}/shadowsocks-v${latest_version}.x86_64-unknown-linux-gnu.tar.xz
 tar xf shadowsocks-v${latest_version}.x86_64-unknown-linux-gnu.tar.xz -C /usr/local/bin
 
-# v2plugin
-vurl='https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest'
+# xray-plugin
+vurl='https://api.github.com/repos/teddysun/xray-plugin/releases/latest'
 latest_version2=`curl $vurl| grep tag_name |awk -F '[:,"v]' '{print $6}'`
-wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v${latest_version2}/v2ray-plugin-linux-amd64-v${latest_version2}.tar.gz
-tar xf v2ray-plugin-linux-amd64-v${latest_version2}.tar.gz -C /usr/local/bin
-mv /usr/local/bin/v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
+wget https://github.com/teddysun/xray-plugin/releases/download/v${latest_version2}/xray-plugin-linux-amd64-v${latest_version2}.tar.gz
+tar xf xray-plugin-linux-amd64-v${latest_version2}.tar.gz -C /usr/local/bin
+mv /usr/local/bin/xray-plugin_linux_amd64 /usr/local/bin/xray-plugin
 
 # creat configfile-folder
 mkdir /etc/shadowsocks-rust >/dev/null 2>&1
@@ -25,8 +25,11 @@ cat > /etc/shadowsocks-rust/config.json <<-EOF
             "address": "127.0.0.1",
             "port": 8388,
             "password": "barfoo!",
+            "timeout":7200,
             "method": "none",
-            "timeout": 7200
+            "plugin":"xray-plugin",
+            "mode":"quic",
+            "plugin_opts":"server;path=/uri"
         }
     ]
 }
@@ -39,7 +42,7 @@ Description=Shadowsocks Server
 After=network.target
 [Service]
 Restart=on-abnormal
-ExecStart=/usr/local/bin/ssserver -c /etc/shadowsocks-rust/config.json -p 443 --plugin v2ray-plugin --plugin-opts "server;mode=quic;host=$ipaddr"
+ExecStart=/usr/local/bin/ssserver -c /etc/shadowsocks-rust/config.json
 StandardOutput=null
 [Install]
 WantedBy=multi-user.target
@@ -49,3 +52,6 @@ systemctl daemon-reload
 systemctl stop ss.service
 systemctl start ss.service
 systemctl enable ss.service
+
+# nginx
+
