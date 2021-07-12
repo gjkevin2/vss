aa43ecc76c829790de9cff09770b1b4fc57a9766
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-07-12 23:42:16 +0800

    Update install-ssrust.sh

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd ~
-rm -rf shadowsocks-v* xray-plugin*
+rm -rf shadowsocks-v* xray-plugin* v2ray-plugin*
 lurl='https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest'
 latest_version=`curl $lurl| grep tag_name |awk -F '[:,"v]' '{print $6}'`
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v${latest_version}/shadowsocks-v${latest_version}.x86_64-unknown-linux-gnu.tar.xz
 tar xf shadowsocks-v${latest_version}.x86_64-unknown-linux-gnu.tar.xz -C /usr/local/bin
 
-# xray-plugin
-vurl='https://api.github.com/repos/teddysun/xray-plugin/releases/latest'
+# v2ray-plugin
+vurl='https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest'
 latest_version2=`curl $vurl| grep tag_name |awk -F '[:,"v]' '{print $6}'`
-wget https://github.com/teddysun/xray-plugin/releases/download/v${latest_version2}/xray-plugin-linux-amd64-v${latest_version2}.tar.gz
-tar xf xray-plugin-linux-amd64-v${latest_version2}.tar.gz -C /usr/local/bin
-mv /usr/local/bin/xray-plugin_linux_amd64 /usr/local/bin/xray-plugin
+wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v${latest_version2}/v2ray-plugin-linux-amd64-v${latest_version2}.tar.gz
+tar xf v2ray-plugin-linux-amd64-v${latest_version2}.tar.gz -C /usr/local/bin
+mv /usr/local/bin/v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
 
 # creat configfile-folder
 mkdir /etc/shadowsocks-rust >/dev/null 2>&1
@@ -26,8 +26,10 @@ cat > /etc/shadowsocks-rust/config.json <<-EOF
             "port": 50003,
             "password": "barfoo!",
             "timeout":7200,
-            "method": "aes-256-gcm",
-            "plugin":"xray-plugin",
+            "method": "none",
+            "fast_open":false,
+            "mode":"tcp_only",
+            "plugin":"v2ray-plugin",
             "plugin_opts":"server;path=/uri"
         }
     ]
