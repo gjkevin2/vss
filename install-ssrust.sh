0c21bc2861a590d1239863cc616033eaa55774e4
gjkevin2 <60083245+gjkevin2@users.noreply.github.com>
2021-07-13 00:05:42 +0800

    Update install-ssrust.sh

--- install-ssrust.sh
+++ install-ssrust.sh
@@ -23,14 +23,15 @@ cat > /etc/shadowsocks-rust/config.json <<-EOF
     "servers": [
         {
             "address": "127.0.0.1",
-            "port": 50003,
+            "nameserver": "8.8.8.8",
+            "server_port": 50003,
             "password": "barfoo!",
-            "timeout":7200,
             "method": "chacha20-ietf-poly1305",
-            "fast_open":false,
-            "mode":"websocket",
-            "plugin":"v2ray-plugin",
-            "plugin_opts":"server;path=/uri"
+            "timeout": 400
+            "no_delay": true,
+            "mode": "tcp_and_udp",
+            "plugin": "v2ray-plugin",
+            "plugin_opts": "server;tls;fast-open;host=s.flyrain.tk;cert=/root/cert/fullchain.cer;key=/root/cert/privkey.key;loglevel=none"
         }
     ]
 }
