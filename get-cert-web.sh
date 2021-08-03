d249631e0844dd51654bf309aa9f604b7d2a13a0
gjkevin <gjkevin2@163.com>
2021-08-03 16:54:55 +0800

    update

--- get-cert-web.sh
+++ get-cert-web.sh
@@ -34,6 +34,7 @@ stream {
         # SNI recognize
         map \$ssl_preread_server_name \$stream_map {
                 x.$domain beforextls;
+                tx.$domain beforetrojanxtls;
                 g.$domain grpc;
                 t.$domain beforetrojan;
                 tg.$domain beforetrojango;
@@ -45,6 +46,12 @@ stream {
         }
         upstream xtls {
                 server 127.0.0.1:50001;
+        }
+        upstream beforetrojanxtls {
+                server 127.0.0.1:50017;
+        }
+        upstream trojanxtls {
+                server 127.0.0.1:1310;
         }
         upstream grpc {
                 server 127.0.0.1:50018;
@@ -83,6 +90,10 @@ stream {
                 proxy_pass xtls;   # redirect to xtls 
         }
         server {
+                listen 127.0.0.1:50017 proxy_protocol;
+                proxy_pass trojanxtls;   # redirect to trojanxtls 
+        }
+        server {
                 listen 127.0.0.1:50012 proxy_protocol;
                 proxy_pass trojan;   # redirect to trojan 
         }
@@ -119,6 +130,11 @@ server {
 server {
         listen 80;
         server_name x.$domain;
+        return 301 https://$domain;
+}
+server {
+        listen 80;
+        server_name tx.$domain;
         return 301 https://$domain;
 }
 server {
