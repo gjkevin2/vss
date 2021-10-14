626c2e7ca195379753e8b592cbba0c989fe36552
gjkevin2 <gjkevin2@163.com>
2021-10-14 11:31:20 +0800

    update

--- get-cert-web.sh
+++ get-cert-web.sh
@@ -161,6 +161,12 @@ server {
         }
 
         location /test { #与vless+grpc应用中serviceName对应
+            if ($content_type !~ "application/grpc"){
+                return 404;
+            }
+            client_max_body_size 0;
+            client_body_timeout 1071906480m;
+            grpc_read_timeout 1071906480m;
             grpc_pass grpc://127.0.0.1:50008; 
         }
 }
