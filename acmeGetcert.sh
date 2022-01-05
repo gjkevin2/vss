fb020265b901e29dac325d00b3d382c30f8e5197
gjkevin2 <gjkevin2@163.com>
2022-01-05 23:18:23 +0800

    update

--- acmeGetcert.sh
+++ acmeGetcert.sh
@@ -3,13 +3,13 @@ apt update && apt -y upgrade && apt -y install socat
 apt -y install curl gawk
 curl https://get.acme.sh | sh -s email=gjkevin@163.com
 ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-export DP_Id='192193'
-export DP_Key='dc85648992cf2d738ee22815979e8a15'
+export Namesilo_Key='f0a93865dc9fa62ec111'
 
 echo '请输入顶级域名'
 read domain
 
-~/.acme.sh/acme.sh --issue -d $domain -d *.$domain --dns dns_dp
+# namesilo 解析太慢，使用900，其他可以不用这个选项
+~/.acme.sh/acme.sh --issue --dns dns_namesilo --dnssleep 900 -d $domain -d *.$domain 
 
 #installcert
 ~/.acme.sh/acme.sh --installcert -d $domain \
@@ -25,4 +25,7 @@ read domain
 
 # restart flask
 source ~/dist/venv/bin/activate
-uwsgi -ini ~/dist/uwsgi/uwsgi.ini
+# uwsgi -ini ~/dist/uwsgi/uwsgi.ini
+pkill -9 uwsgi
+systemctl stop uclient
+systemctl start uclient
