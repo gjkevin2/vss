169b4c8f45cce90892c00e6080687c597c7ae206
gjkevin2 <gjkevin2@163.com>
2022-01-10 16:05:11 +0800

    update

--- centos7/bbrplus.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget "https://github.com/a8866051/bbrplus/raw/master/ok_bbrplus_centos.sh" && chmod +x ok_bbrplus_centos.sh && ./ok_bbrplus_centos.sh
--- centos7/install-sslibev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- # wget –no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh 
- # wget –no-check-certificate -O shadowsocks-libev.sh  https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
- wget –no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/gjkevin2/vss/master/shadowsocks-libev.sh
- chmod +x shadowsocks-libev.sh 
- ./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log
