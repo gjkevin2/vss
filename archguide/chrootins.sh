e6660c9534a8bf3251033c2be5f3cfa33558d4f3
gjkevin <gjkevin2@163.com>
2022-10-06 01:04:48 +0800

    update

--- archguide/chrootins.sh
+++ archguide/chrootins.sh
@@ -33,7 +33,7 @@ systemctl enable iwd
 mkdir /etc/iwd 2>/dev/null
 # chain to /etc/resolv.conf
 rm -rf /etc/resolv.conf
-ln -sf /run/systemd/network/resolv.conf /etc/resolv.conf
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 cat >/etc/iwd/main.conf<<-EOF
 [General]
 EnableNetworkConfiguration=true
