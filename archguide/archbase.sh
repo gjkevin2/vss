fb98719244e4cfeb2db49071faa60c46b65b2cba
gjkevin2 <gjkevin2@163.com>
2022-11-28 00:49:45 +0800

    update

--- archguide/archbase.sh
+++ archguide/archbase.sh
@@ -11,15 +11,33 @@ read lp
 mkfs.ext4 $lp
 mount $lp /mnt
 
+# stop reflector
+systemctl stop reflector
 # curl 'https://archlinux.org/mirrorlist/?country=CN' | sed -e 's/#Server/Server/' -e '/^#/d'>/etc/pacman.d/mirrorlist
-echo -e "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch">/etc/pacman.d/mirrorlist
+# echo -e "Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch">/etc/pacman.d/mirrorlist
+cat >/etc/pacman.d/mirrorlist<<\EOF
+# China
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.aliyun.com/archlinux/$repo/os/$arch
+Server = https://mirrors.sjtug.sjtu.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.cqu.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.neusoft.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.nju.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.njupt.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirror.redrock.team/archlinux/$repo/os/$arch
+Server = https://mirrors.shanghaitech.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.wsyu.edu.cn/archlinux/$repo/os/$arch
+Server = https://mirrors.xjtu.edu.cn/archlinux/$repo/os/$arch
+EOF
 #echo -e "Server = http://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch">/etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 #cat /mnt/etc/fstab
 
-# set mirrorlist
-reflector --country China --age 24 --protocol https --sort rate --save /mnt/etc/pacman.d/mirrorlist
+
+# reflector --country China --age 24 --protocol https --sort rate --save /mnt/etc/pacman.d/mirrorlist
 
 #add user:passwd to a file
 echo "$ura">/mnt/newu
