b58e5d2ea929e5e25212ba4fbfc2213d2c0052b9
gjkevin <gjkevin2@163.com>
2023-01-06 23:44:02 +0800

    update

--- TlsFakeSet.sh
+++ TlsFakeSet.sh
@@ -7,7 +7,7 @@ curl https://get.acme.sh | sh -s email=gjkevin@163.com
 #export DP_Key='dc85648992cf2d738ee22815979e8a15'
 export CF_Key="b8a4d01054c1e780d4f8346a302c5ae0e988d" 
 export CF_Email="gjkevin2@163.com"
-acme.sh --register-account -m gjkevin2@163.com --server zerossl
+~/.acme.sh/acme.sh --register-account -m gjkevin2@163.com --server zerossl
 
 echo '请输入顶级域名'
 read domain
