127285fee05169406647b09cc9d0c96cd8aaa109
gjkevin <gjkevin2@163.com>
2023-01-10 22:12:02 +0800

    update

--- install-xray.sh
+++ install-xray.sh
@@ -178,8 +178,9 @@ cat > /usr/local/etc/xray/config.json <<-EOF
       "port": 10630,
       "protocol": "shadowsocks",
       "settings": {
-        "method": "aes-256-gcm",
-        "password": "barfoo!"
+        "method": "2022-blake3-aes-128-gcm",
+        "password": "barfoo!",
+        "network": "tcp,udp"
       }
     }
   ],
