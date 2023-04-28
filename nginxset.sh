94ff1c9ea9ff4696d7a9069065678599b135c75d
gjkevin2 <gjkevin2@163.com>
2023-04-28 08:36:55 +0800

    update

--- nginxset.sh
+++ nginxset.sh
@@ -3,7 +3,7 @@ echo '请输入顶级域名'
 read domain
 
 # set sni bypass
-serverip=$(ip addr|grep inet|grep -v 127.0.0.1|grep -v inet6|awk -F '/' '{print $1}'|tr -d "inet ")
+# serverip=$(ip addr|grep inet|grep -v 127.0.0.1|grep -v inet6|awk -F '/' '{print $1}'|tr -d "inet ")
 # nginx need user root to use unix socket
 # sed -i 's/user.*/user  root/g' /etc/nginx/nginx.conf
 # sed -i 's#/var/run/nginx.pid#/run/nginx.pid#g' /etc/nginx/nginx.conf
@@ -31,8 +31,8 @@ stream {
 EOF
 
 # ssl相关配置及realip设置
-grep "ssl_session_timeout" /etc/nginx/nginx.conf || {
-  sed -i "/keepalive_timeout/a\\\tssl_session_cache shared:SSL:10m;\n\tssl_session_timeout 10m;\n\tset_real_ip_from 0.0.0.0/0;\n\treal_ip_header X-Forwarded-For;\n\treal_ip_recursive on;" /etc/nginx/nginx.conf
+grep "ssl_certificate" /etc/nginx/nginx.conf || {
+  sed -i "/keepalive_timeout/a\\\tssl_certificate \/root\/cert\/fullchain.cer;\n\tssl_certificate_key \/root\/cert\/$domain.key;\n\tset_real_ip_from 127.0.0.1;\n\treal_ip_header proxy_protocol;" /etc/nginx/nginx.conf
 }
 
 
@@ -40,18 +40,16 @@ cat >/etc/nginx/conf.d/default.conf<<-EOF
 server {
     listen 80;
     listen [::]:80;
+    server_name ~^(www\.)?(.+)$;
     return 301 https://\$host\$request_uri;
 }
 
 server {
     listen unix:/dev/shm/web.sock ssl http2 proxy_protocol;
-    server_name ~^(?<www>www\.)?(.+)$; 
-    if (\$www) {return 301 https://\$2\$request_uri;}
-
-    ssl_certificate /root/cert/fullchain.cer; 
-    ssl_certificate_key /root/cert/$domain.key;
-    ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
-    ssl_ciphers HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4:!RSA;
+    server_name $domain www.$domain;
+    port_in_redirect off;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305;
     add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
 
     location / {
@@ -64,4 +62,5 @@ EOF
 
 # (re)start nginx
 systemctl stop nginx
+rm -rf /dev/shm/*
 systemctl start nginx
