0870e49f8c81f0b567e4050ee340d50cf71e679d
gjkevin <gjkevin2@163.com>
2024-01-02 20:59:43 +0800

    update

--- install-singbox.sh
+++ install-singbox.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #安装
 # 测试版
- # bash -c "$(curl -L https://sing-box.vercel.app)" @ install --beta
- bash -c "$(curl -L https://sing-box.vercel.app)" @ install
+ bash -c "$(curl -L https://sing-box.vercel.app)" @ install --beta
+ # bash -c "$(curl -L https://sing-box.vercel.app)" @ install
+ # bash -c "$(curl -L sing-box.vercel.app)" @ remove
 
 # 获取ip和域名
 serverip=$(ip addr|grep inet|grep -v 127.0.0.1|grep -v inet6|awk -F '/' '{print $1}'|tr -d "inet ")
@@ -16,7 +17,6 @@ servername=${a##*.}.${testdomain##*.}
 # sing-box generate reality-keypair
 # reality的public key： Y8cBV8RyH9hSkdy0ATDC9T4s0hzMq1rUXU_YmH6Fgj4
 # 偷证书的网站必须支持TLSv1.3和H2
-# 注意proxy_protocol的设置，要和nginx一致
 cat > /usr/local/etc/sing-box/config.json <<-EOF
 {
     "inbounds": [
@@ -49,8 +49,6 @@ cat > /usr/local/etc/sing-box/config.json <<-EOF
             "type": "vless",
             "listen": "127.0.0.1",
             "listen_port": 52004,
-            "proxy_protocol": true,
-            "proxy_protocol_accept_no_header": false,
             "users": [
                 {
                     "uuid": "461edf16-8619-4f46-8e1e-e3994c8c00a2",
@@ -89,10 +87,12 @@ systemctl stop nginx
 #systemctl daemon-reload
 rm -rf /dev/shm/*
 
-# 443端口转发到实际端口
+# 443端口转发到实际端口，nginx还要移除proxy_protocol
 grep "upstream singbox-reality" /etc/nginx/nginx.conf || {
-  sed -i "/\$ssl_preread_server_name/a\\\t\twww.microsoft.com singbox-reality;" /etc/nginx/nginx.conf
+  sed -i "/\$ssl_preread_server_name/a\\\t\twww.microsoft.com singbox-proxy;" /etc/nginx/nginx.conf
   sed -i "/upstream set/a\\\tupstream singbox-reality {\n\t\tserver 127.0.0.1:52004;\n\t}" /etc/nginx/nginx.conf
+  sed -i "/upstream set/a\\\tupstream singbox-proxy {\n\t\tserver 127.0.0.1:52204;\n\t}" /etc/nginx/nginx.conf
+  sed -i "/remove proxy_protocol/a\\\tserver {\n\t\tlisten 52204 proxy_protocol;\n\t\tproxy_pass singbox-reality;\n\t}" /etc/nginx/nginx.conf
 }
 
 # (re)start nginx
