c827f6293b7dd726688c65f449b841a488850510
gjkevin <gjkevin2@163.com>
2024-07-02 00:45:46 +0800

    update

--- updateCertForWeb.sh
+++ updateCertForWeb.sh
@@ -4,5 +4,5 @@ echo '请输入顶级域名'
 read domain
 ~/.acme.sh/acme.sh --install-cert -d $domain \
 --key-file       $HOME/cert/$domain.key  \
---fullchain-file $HOME/cert/fullchain.cer
+--fullchain-file $HOME/cert/fullchain.cer \
 --reloadcmd     "service nginx force-reload"
