52a03b089c13285eb587f82da3d37ca565bed9ad
gjkevin <gjkevin2@163.com>
2025-02-20 20:23:00 +0800

    update

--- metaso-nginx.sh
+++ metaso-nginx.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# deploy free-api
+docker run -it -d --init --name metaso-free-api -p 8000:8000 -e TZ=Asia/Shanghai vinlic/metaso-free-api:latest
+# docker logs -f metaso-free-api
+# docker restart metaso-free-api
+# docker stop metaso-free-api
+
 domain=158742.xyz
 
 cat >/etc/nginx/conf.d/metaso.conf<<-EOF
